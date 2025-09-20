09dbce451d3833b6e6834585c72afb464f34c726
Jannik <jannikver@gmail.com>
2025-09-20 22:50:18 +0900

    D: adding letter_count function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from stats import word_count
+from stats import word_count, letter_count
 
 frank = "books/frankenstein.txt"
+
 def get_book_text(path_file):
     with open(path_file) as f:
         file_content = f.read()
@@ -8,5 +9,5 @@ def get_book_text(path_file):
 
 def main():
     print(word_count(frank))
-
+    print(letter_count(frank))
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def word_count(path_file):
         file_content = f.read()
     word_count = len(file_content.split())
     return f"{word_count} words found in the document"
+
+def letter_count(path_file):
+    with open(path_file) as f:
+        file_content = list(f.read().lower())
+    letter_count = {}
+    for i in file_content:
+        if i not in letter_count:
+            letter_count[i]=0
+    for i in file_content:
+        letter_count[i] += 1
+    return letter_count
