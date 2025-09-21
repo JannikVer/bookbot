f73d393d0c15175827e318be646e9f4b26eebcd6
Jannik <jannikver@gmail.com>
2025-09-21 13:52:05 +0900

    F: adding letter_sort function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, letter_count
+from stats import word_count, letter_count, letter_sort
 
 def get_book_text(path_file):
     with open(path_file) as f:
@@ -9,6 +9,17 @@ def main():
     file_content = get_book_text("books/frankenstein.txt")
     Word_count = word_count(file_content)
     Letter_count = letter_count(file_content)
+    sorted_letter = letter_sort(Letter_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
     print(Word_count)
-    print(Letter_count)
+    print("--------- Character Count -------")
+    
+    for letter in sorted_letter:
+        print(f"{letter["char"]}: {letter["num"]}")
+
+    print("============= END ===============")
+
+    
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,9 @@
 def word_count(file):
     word_count = len(file.split())
-    return f"{word_count} words found in the document"
+    return f"Found {word_count} total words"
 
-def letter_count(file):
+def letter_count(File):
+    file = File.lower()
     letter_count = {}
     for i in file:
         if i not in letter_count:
@@ -10,3 +11,17 @@ def letter_count(file):
     for i in file:
         letter_count[i] += 1
     return letter_count
+
+def sort_on(items):
+    return items["num"]
+
+def letter_sort(dic):
+    letters = []
+    for letter in dic:
+        if letter.isalpha():
+            char = {}
+            char["char"] = letter
+            char["num"] = dic[letter]
+            letters.append(char)
+    letters.sort(reverse=True, key=sort_on)
+    return letters
